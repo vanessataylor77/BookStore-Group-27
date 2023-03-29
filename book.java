afafef5e7014a394346431ffd61a6d3fb1942e1e
vanessataylor77 <125626537+vanessataylor77@users.noreply.github.com>
2023-03-28 20:20:04 -0400

    Create book.java

---
+++ book.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author vrtaylor
+ */
+@Entity
+@Table(name = "books")
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String author;
+
+    @Column(nullable = false)
+    private Double price;
+
+    // Constructors, getters, and setters
+}
